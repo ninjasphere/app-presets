27017f84a87f678c606ff1b4270bf4e9e773c5b0
Jon Seymour <jon@ninjablocks.com>
2015-02-09 23:53:58 +1100

    Add JSON model for scenes.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

--- model/model.go
+++ model/model.go
@@ -1,8 +1,34 @@
-// Package model describes a collection of tasks to be executed at the opening and closing
-// of schedule windows.
+// Presets is the configuration for the app-presets app. It consists of
 package model
 
-// A Schedule specifies a list of Tasks, a Location and a TimeZone
+// A ChannelState represents the state of a single channel.
+type ChannelState struct {
+	ID    string      `json:"id,omitempty"`
+	State interface{} `json:"state,omitempty"`
+}
+
+// A ThingState represents the state of a single thing. It consists of the id of the thing,
+// a list of channel states and a boolean which indicates whether the thing is
+// included in the scene.
+type ThingState struct {
+	ID       string         `json:"id,omitempty"`
+	Channels []ChannelState `json:"channels"`
+}
+
+// A Scene encodes the state of multiple things within a scope. It has a UUID that is a unique
+// identifier the scene, a slot number, which is the position of the scene within a
+// UI menu, a label which provides a human readable label for a scene, a scope which restricts the
+// set of selectable things and a list of thing states.
+type Scene struct {
+	UUID   string       `json:"uuid,omitempty"`
+	Slot   int          `json:"slot,omitempty"`
+	Label  string       `json:"label,omitempty"`
+	Scope  string       `json:"scope,omitempty"`
+	Things []ThingState `json:"things"`
+}
+
+// A Presets object is a collection of Scenes.
 type Presets struct {
-	Version string `json:"version,omitempty"`
+	Version string  `json:"version,omitempty"`
+	Scenes  []Scene `json:"scenes"`
 }
---
+++ model/model_test.go
@@ -0,0 +1,62 @@
+package model
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+func assert(t *testing.T, description string, assertion func() bool) {
+	if !assertion() {
+		t.Fatalf("assertion failed: %s\n", description)
+	}
+}
+
+func TestJSONRoundTrip(t *testing.T) {
+	item := &Presets{
+		Version: "1.0",
+		Scenes: []Scene{
+			Scene{
+				UUID:  "CAFE-BABE-0001",
+				Slot:  1,
+				Label: "Romantic",
+				Scope: "sites/a458dfe3-3a81-43cc-a118-6c42c814f4b3",
+				Things: []ThingState{
+					ThingState{
+						ID: "ba8236f9-a813-11e4-8ab9-7c669d02a706",
+						Channels: []ChannelState{
+							ChannelState{
+								ID:    "on-off",
+								State: false,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	serialized, err := json.Marshal(item)
+	if err != nil {
+		t.Fatalf("marhsalling - %s", err)
+	}
+	deserialized := &Presets{}
+	err = json.Unmarshal(serialized, deserialized)
+	if err != nil {
+		t.Fatalf("unmarhsalling - %s", err)
+	}
+
+	assert(t, "uuid", func() bool { return deserialized.Scenes[0].UUID == item.Scenes[0].UUID })
+	assert(t, "deserialized.Scenes[0].Things[0].Channels[0].State", func() bool { ret`urn deserialized.Scenes[0].Things[0].Channels[0].State == false })
+
+}
+
+func TestDeserialization(t *testing.T) {
+	serialized := []byte("{\"version\":\"1.0\",\"scenes\":[{\"uuid\":\"CAFE-BABE-0001\",\"slot\":1,\"label\":\"Romantic\",\"scope\":\"sites/a458dfe3-3a81-43cc-a118-6c42c814f4b3\",\"things\":[{\"id\":\"ba8236f9-a813-11e4-8ab9-7c669d02a706\",\"channels\":[{\"id\":\"on-off\",\"state\":{\"a\":\"b\", \"c\": 2}}]}]}]}")
+	deserialized := &Presets{}
+	err := json.Unmarshal(serialized, deserialized)
+	if err != nil {
+		t.Fatalf("unmarhsalling - %s", err)
+	}
+	log.Printf("%v", deserialized.Scenes[0].Things[0].Channels[0].State)
+}
