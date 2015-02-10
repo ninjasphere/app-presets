f5135bc40891e7aad45746fb76c7dbc1adef370e
Jon Seymour <jon@ninjablocks.com>
2015-02-10 12:41:16 +1100

    Define the service interface.
    Signed-off-by: Jon Seymour <jon@ninjablocks.com>

---
+++ service/service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"fmt"
+	"github.com/ninjasphere/app-presets/model"
+)
+
+type PresetsService struct {
+}
+
+func (ps *PresetsService) Init() {
+}
+
+// see: http://schema.ninjablocks.com/service/presets#listPresetable
+func (ps *PresetsService) ListPresetable(scope string) ([]model.ThingState, error) {
+	return make([]model.ThingState, 0, 0), fmt.Errorf("unimplemented function: ListPresetable")
+}
+
+// see: http://schema.ninjablocks.com/service/presets#fetchScenes
+func (ps *PresetsService) FetchScenes(scope string) ([]model.Scene, error) {
+	return make([]model.Scene, 0, 0), fmt.Errorf("unimplemented function: FetchScenes")
+}
+
+// see: http://schema.ninjablocks.com/service/presets#fetchScene
+func (ps *PresetsService) FetchScene(id string) (*model.Scene, error) {
+	return nil, fmt.Errorf("unimplemented function: FetchScene")
+}
+
+// see: http://schema.ninjablocks.com/service/presets#storeScene
+func (ps *PresetsService) StoreScene(model *model.Scene) error {
+	return fmt.Errorf("unimplemented function: StoreScene")
+}
+
+// see: http://schema.ninjablocks.com/service/presets#applyScene
+func (ps *PresetsService) ApplyScene(id string) error {
+	return fmt.Errorf("unimplemented function: ApplyScene")
+}
